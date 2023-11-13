127aab8a74927b04f299386664c9f0ca406d37e9
Lucas <lucas.dias@duque.g12.br>
2023-11-13 19:48:41 -0300

    segundo commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {React, useState, useEffect} from 'react'
+import { StyleSheet, Text, View, TouchableOpacit, detox } from 'react-native';
 
 export default function App() {
+  const titulos = ['Quanto é 2+2','Quanto é 2+3', 'Quanto é 2/2', 'Quanto é 50+50']
+  const respostas = ['Resposta: 4', 'Resposta: 5', 'Resposta: 1', 'Resposta: 100']
+  const [quiz, setQuiz] = useState(0);
+  const [resposta, setResposta] = useState(0);
+
+  useEffect(() => {
+    document.title = titulos[quiz];
+  }, [quiz])
+
+  useEffect(() => {
+    document.title = respostas[resposta];
+  }, [resposta])
+
+  const botaoEvento = () => {
+    setQuiz((prevQuiz) => (prevQuiz + 1) % titulos.length);
+  };
+  const botaoEventoResposta = () => {
+    setQuiz((prevResposta) => (prevResposta + 1) % respostas.length);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <h1>QUIZ</h1>
+      <p>{titulos[quiz]}</p>
+      <p>Digite sua Resposta</p><input type="text" />
+      <p>Resposta correta: </p>
+     <button onClick={botaoEvento}>Proxima questao</button>
     </View>
   );
 }
